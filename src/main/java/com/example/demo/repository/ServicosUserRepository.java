a1d2f1619f84ef6d0250cb793ed6452e90884391
Lucas Ap <lukasap724@gmail.com>
2024-02-19 19:25:46 -0300

    recpmeço

--- src/main/java/com/example/demo/repository/ServicosUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.user.servicoUser.ServiceUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicosUserRepository extends JpaRepository<ServiceUser, Long> {
-}
