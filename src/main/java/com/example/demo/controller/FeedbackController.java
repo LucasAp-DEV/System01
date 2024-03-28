9f906f9d0e98fd49680ac8f334d0678dce397578
LucasAp-DEV <lucasapdev@gmail.com>
2024-03-28 16:13:28 -0300

    mudança service

--- src/main/java/com/example/demo/controller/FeedbackController.java
+++ src/main/java/com/example/demo/controller/FeedbackController.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.domain.feedback.Feedback;
 import com.example.demo.domain.feedback.FeedbackDTO;
 import com.example.demo.repository.FeedbackRepository;
+import com.example.demo.service.FeedbackServide;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
@@ -18,23 +20,29 @@ public class FeedbackController {
     @Autowired
     private FeedbackRepository repository;
 
+    @Autowired
+    private FeedbackServide service;
+
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody @Valid FeedbackDTO data) {
-        Feedback newFeedback = new Feedback(data.descricao(), data.nota(), data.localId());
-        this.repository.save(newFeedback);
-        return ResponseEntity.ok().build();
+    public ResponseEntity register(@RequestBody Feedback data) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(service.saveFeedback(data));
+//        Feedback newFeedback = new Feedback(data.descricao(), data.nota(), data.localId());
+//        this.repository.save(newFeedback);
+//        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity deleteFeedbac(@RequestBody @Valid FeedbackDTO data) {
+    public ResponseEntity<Object> deleteFeedbac(@PathVariable(value = "id")Long id, @RequestBody @Valid Feedback feedback) {
+        Optional<Feedback> dell = service.returnId(id);
 
-        Optional<Feedback> deleteFeedback = repository.findById((data.id()));
-        if(deleteFeedback.isEmpty()) {
-            this.repository.deleteById(data.id());
-            return ResponseEntity.ok().build();
-        }else {
+//        Optional<Feedback> deleteFeedback = repository.findById((data.id()));
+
+        if (dell.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+        service.dellFeedback(dell);
+        return ResponseEntity.status(HttpStatus.OK).body("Feedback Deletado");
 
     }
 }
---
+++ src/main/java/com/example/demo/service/FeedbackServide.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.feedback.Feedback;
+import com.example.demo.domain.feedback.FeedbackDTO;
+import com.example.demo.repository.FeedbackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class FeedbackServide {
+
+    @Autowired
+    private FeedbackRepository repository;
+
+    public Feedback saveFeedback (Feedback feedback) {
+        return repository.save(feedback);
+    }
+
+    public Optional<Feedback> returnId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void dellFeedback(Optional<Feedback> feedback) {
+        repository.delete(feedback.get());
+    }
+
+}
