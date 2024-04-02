7ba2b9e87cf24bb0a76bd7d0587cf34798581080
LucasAp-DEV <lucasapdev@gmail.com>
2024-04-02 17:05:00 -0300

    mudança service Image Enconpleto

--- src/main/java/com/example/demo/controller/ImageController.java
+++ src/main/java/com/example/demo/controller/ImageController.java
@@ -30,39 +30,25 @@ public class ImageController {
             service.saveImage(image);
             return ResponseEntity.ok().body("Imagen Cadastrada");
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Imagen não cadastrada");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Imagen não cadastrada");
         }
     }
 
 
-
-//    @PutMapping("/update")
-//    public ResponseEntity updateImage(@RequestBody UpdateImageDTO data) {
+//    @DeleteMapping("/delete")
+//    public ResponseEntity deleteImage(@RequestBody UpdateImageDTO data) {
 //        Optional<Image> optionalImage = imageRepository.findById(data.id());
 //        if (optionalImage.isPresent()) {
-//            Image image = optionalImage.get();
-//            image.setImage(data.image());
-//            this.imageRepository.save(image);
-//            return ResponseEntity.status(HttpStatus.OK).body("Image updated successfully");
+//            this.imageRepository.deleteById(data.id());
+//            return ResponseEntity.status(HttpStatus.OK).body("Imagen Deletada");
 //        } else {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Image not found");
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Imagen Não Deletada");
 //        }
 //    }
-
-    @DeleteMapping("/delete")
-    public ResponseEntity deleteImage(@RequestBody UpdateImageDTO data) {
-        Optional<Image> optionalImage = imageRepository.findById(data.id());
-        if (optionalImage.isPresent()) {
-            this.imageRepository.deleteById(data.id());
-            return ResponseEntity.status(HttpStatus.OK).body("Imagen Deletada");
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Imagen Não Deletada");
-        }
-    }
-
-    @GetMapping("/list")
-    public ResponseEntity<List<Image>> getAllImages() {
-        List<Image> images = imageRepository.findAll();
-        return ResponseEntity.status(HttpStatus.OK).body(images);
-    }
+//
+//    @GetMapping("/list")
+//    public ResponseEntity<List<Image>> getAllImages() {
+//        List<Image> images = imageRepository.findAll();
+//        return ResponseEntity.status(HttpStatus.OK).body(images);
+//    }
 }
