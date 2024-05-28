fe428950cd21fceb6e5b3a8fded4cad9779e6f30
LucasAp-DEV <lucasapdev@gmail.com>
2024-05-28 10:57:06 -0300

    expiração do token

--- src/main/java/demo/TCC/infra/TokenService.java
+++ src/main/java/demo/TCC/infra/TokenService.java
@@ -48,6 +48,6 @@ public class TokenService {
     }
 
     private Instant genExpirationDate() {
-        return LocalDateTime.now().plusHours(10).toInstant(ZoneOffset.of("-03:00"));
+        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-03:00"));
     }
 }
